14a3933f4f2a4f5aa4fcae4594b95e203022113b
balberto2013 <balberto2013@c6433d9f-91b6-190b-5e11-e836ff05c902>
2014-10-08 23:51:23 +0000

    Vidage

--- trunk/src/aleatoire.ads
+++
@@ -1,3 +0,0 @@
-with Ada.Numerics.Discrete_Random;
--- renvoi un nombre aleatoire dans [ A , B ]
-function Aleatoire(A:Integer ; B:integer) return Integer;
--- trunk/src/tictactoe-ia.ads
+++
@@ -1,5 +0,0 @@
-package Tictactoe.IA is
-   procedure Facile(P_Plateau : in out Plateau ; P_Pion : Pion);
-   procedure Moyen(P_Plateau : in out Plateau ; P_Pion : Pion);
-   procedure Difficile(P_Plateau : in out Plateau ; P_Pion : Pion);
-end Tictactoe.IA;
