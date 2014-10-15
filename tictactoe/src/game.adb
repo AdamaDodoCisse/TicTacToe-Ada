01cb694e4a3c1c55739ae6e2d7759531774485e0
dylan.njock@gmail.com <dylan.njock@gmail.com@c6433d9f-91b6-190b-5e11-e836ff05c902>
2014-10-15 19:10:14 +0000

    Rectification du probleme du match nul, et commentaire du code.

--- tictactoe/src/game.adb
+++ tictactoe/src/game.adb
@@ -11,31 +11,32 @@ Package body Game is
 
    procedure Affichage(P_Plateau : Tictactoe.Plateau.Pointeur_Plateau) is
    begin
-      For i in ligne loop
-         For j in Colonne loop
-            Put("|");
+      For i in ligne loop-- Pour chaque ligne du plateau
+         For j in Colonne loop-- Pour chaque colonne du plateau
+            Put("|"); -- On affiche un | pour marquer la séparation entre les cases du plateau
             if Tictactoe.Cellule.EstLibre(Tictactoe.Plateau.Get_Cellule(P_Plateau,i,j)) then
-               Put(" ");
-            else
+               --Si la celule est vide
+               Put(" ");-- On affiche " "
+            else --Sinon
                Put(Pion'Image(Get_Pion(Tictactoe.Plateau.Get_Cellule(P_Plateau,i,j))));
+               --On affiche le pion de cette Cellule en utilisant Get_Pion
             end if;
          end loop;
-         Put_Line("|");
+         Put_Line("|");-- On affiche un  | à chaque fois que l'on fini de parcourir une ligne.
       end loop;
    end Affichage;
 
    procedure Jouer_Tour(P : Tictactoe.Plateau.Pointeur_Plateau;J:Joueur) is
    begin
-      case Get_Type(J) is
-      When 1=>
-         Reel(P,J,Get_Pion(J));
-      when 2=>
-
-         Facile(P,Get_Pion(J));
-      when 3=>
-         Moyen(P,Get_Pion(J));
-      when 4=>
-         Difficile(P,Get_Pion(J));
+      case Get_Type(J) is-- En fonction du type du joueur
+      When 1=>-- Si c'est un Humain
+         Reel(P,J,Get_Pion(J));-- On laisse l'utilisateur jouer
+      when 2=>--Si c'est un CPU de niveau Facile
+         Facile(P,Get_Pion(J));--On appelle la fonction Facile pour jouer
+      when 3=>--Si c'est un CPU de niveau Moyen
+         Moyen(P,Get_Pion(J));--On appelle la fonction Moyen pour jouer
+      when 4=>-- Si c'est un CPU de niveau Difficile
+         Difficile(P,Get_Pion(J));--On appelle la fonction Difficile pour jouer
       end case;
    end Jouer_Tour;
 
@@ -46,12 +47,17 @@ Package body Game is
       For i in 1..2 loop
          Put_Line(ASCII.ESC & "[2J");
          Put_Line("Joueur numéro " & Integer'Image(i) & " Saisissez votre nom");
+         --On demande à l'utilisateur de saisir son nom
          case i is
             when 1=>
                Set_Name(J1,To_Unbounded_String(Get_Line));
+               --On initialise le nom du joueur
                Set_Id(J1,i);
+               --Son id est autmatiquement initialisé en fonction de i
                Set_Type(J1);
+               --On appelle la procedure pour initialiser le type du joueur
                J.Tab_j(i):=J1;
+               --On ajoute ce joueur au tableau
             when 2=>
                Set_Name(J2,To_Unbounded_String(Get_Line));
                Set_Id(J2,i);
@@ -63,32 +69,40 @@ Package body Game is
    end Inscrire_Joueurs;
 
    procedure Jouer is
-            J:Jeu;
+      J:Jeu;
       cpt:Integer;
       choice : integer;
    begin
-      cpt:=0;
-      J.P:=NouveauPlateau;
-      Inscrire_Joueurs(J);
+      cpt:=0;--Compteur de tour jouer
+      J.P:=NouveauPlateau;--Initialise le plateau du jeu
+      Inscrire_Joueurs(J);--On inscrit les joueurs pour la partie
       while EstPlein(J.P)=False and not Gagnant(J.P,Get_Pion(J.Tab_j(1))) and not Gagnant(J.P,Get_Pion(J.Tab_j(2))) loop
-         Affichage(J.P);
-         Jouer_Tour(J.P,J.Tab_j((cpt mod 2)+1));
-         Put(ASCII.ESC & "[2J");
-         cpt:=cpt+1;
+         --Tant que le plateau n'est pas plein et que le joueur 1 n'est pas gagnant et que le joueur 2 n'est pas gagnant.
+         Affichage(J.P);-- Affichage du plateau
+         Jouer_Tour(J.P,J.Tab_j((cpt mod 2)+1));--On fait jouer le joueur courant.
+         --(cpt mod 2) + 1 permet de savoir à qui est-ce le tour de jouer.
+         Put(ASCII.ESC & "[2J");-- Efface le contenu de la console
+         cpt:=cpt+1;--On incrémente le compteur de tour.
       end loop;
       Affichage(J.P);
       if Gagnant(J.P,Get_Pion(J.Tab_j(1))) or Gagnant(J.P,Get_Pion(J.Tab_j(2))) then
+         --Si il y a un gagnant
          if Gagnant(J.P,Get_Pion(J.tab_j(1))) then
+            --Si c'est le joueur 1
             put_line( to_string(get_name(j.tab_j(1))) & " a gagné");
-
+            --On indique que c'est lui qui à gagné.
          else
             put_line(to_string(get_name(j.tab_j(2))) & " a gagné");
          end if ;
-         put_line("voulez-vous refaire une partie ? 1=> oui ; 0=> non  ");
-         choice := integer'value(get_line);
-         if choice = 1 then
-            Jouer;
-         end if ;
+      else--Sinon on affiche "Match Nul"
+         put_line("Match nul......");
+      end if ;
+      put_line("voulez-vous refaire une partie ? 1=> oui ; 0=> non  ");
+      --On demande à l'utilisateur si il veut rejouer
+      choice := integer'value(get_line);
+      if choice = 1 then
+         --Si il saisit 1 alors on rappelle la fonction jouer pour relancer.
+         Jouer;
       end if ;
    end Jouer;
 
--- tictactoe/src/game.ads
+++ tictactoe/src/game.ads
@@ -19,15 +19,25 @@ use Tictactoe.Cellule;
 
 Package Game is
    Type Jeu is private;
-   Type Tableau_J is array(1..2) of Joueur;
+   Type Tableau_J is array(1..2) of Joueur;--Un tableau pour stocker les 2 joueurs de la partie
+
+   --Affiche le plateau
    procedure  Affichage(P_Plateau: Tictactoe.Plateau.Pointeur_Plateau);
+
+   --Permet de lancer une partie
    procedure Jouer;
+
+   --Permet d'obtenir le pion associer à un joueur
    Function Get_Pion(J:Joueur) return Pion;
+
+   --Permet à un joueur de jouer un coup
    procedure Jouer_Tour(P:Tictactoe.Plateau.Pointeur_Plateau;J:Joueur);
+
+   --Procedure de stockage des joueurs dans le jeu
    Procedure Inscrire_Joueurs(J:in out Jeu);
 private
    Type Jeu is record
-      P: Tictactoe.Plateau.Pointeur_Plateau;
-      Tab_j:Tableau_J;
+      P: Tictactoe.Plateau.Pointeur_Plateau;-- Le plateau du jeu
+      Tab_j:Tableau_J; --Le tableau des joueurs de la partie
    end record;
 end Game;
